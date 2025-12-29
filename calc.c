60b3fe5889806c9764aa976577639253d6e179ad
nana1291590712 <162336176+nana1291590712@users.noreply.github.com>
2025-12-29 18:54:55 +0800

    Add files via upload

--- calc.c
+++ calc.c
@@ -1,31 +1,92 @@
 #include "calc.h"
-#include <stdlib.h>
 
-double calc_eval(const char *s)
+/* ??????(????) */
+static double parse_number(const char *s, int *i)
 {
-    double result = 0.0;
-    double value = 0.0;
-    char op = 0;
+    double v = 0.0;
+    double frac = 0.0;
+    double base = 0.1;
+    int dot = 0;
+
+    while ((s[*i] >= '0' && s[*i] <= '9') || s[*i] == '.')
+    {
+        if (s[*i] == '.')
+        {
+            dot = 1;
+        }
+        else if (!dot)
+        {
+            v = v * 10 + (s[*i] - '0');
+        }
+        else
+        {
+            frac += (s[*i] - '0') * base;
+            base *= 0.1;
+        }
+        (*i)++;
+    }
+    return v + frac;
+}
+
+/* ?????? */
+static int precedence(char op)
+{
+    if (op == '+' || op == '-') return 1;
+    if (op == 'x' || op == '/') return 2;
+    return 0;
+}
+
+/* ?????? */
+static double apply(double a, double b, char op)
+{
+    if (op == '+') return a + b;
+    if (op == '-') return a - b;
+    if (op == 'x') return a * b;
+    if (op == '/') return (b != 0) ? a / b : 0.0;
+    return 0.0;
+}
+
+/* ======================
+ * ??????(????)
+ * ====================== */
+double calc_eval(const char *expr)
+{
+    double numStack[16];
+    char   opStack[16];
+    int nTop = -1;
+    int oTop = -1;
     int i = 0;
 
-    /* ???? */
-    result = atof(s);
-
-    /* ??????? */
-    while ((s[i] >= '0' && s[i] <= '9') || s[i] == '.') i++;
-
-    while (s[i] != '\0')
+    while (expr[i])
     {
-        op = s[i++];
-        value = atof(&s[i]);
+        /* ????? */
+        numStack[++nTop] = parse_number(expr, &i);
 
-        while ((s[i] >= '0' && s[i] <= '9') || s[i] == '.') i++;
+        /* ????????? */
+        if (expr[i])
+        {
+            char op = expr[i++];
 
-        if (op == '+') result += value;
-        else if (op == '-') result -= value;
-        else if (op == 'x') result *= value;
-        else if (op == '/') result /= value;
+            /* ????? */
+            while (oTop >= 0 &&
+                   precedence(opStack[oTop]) >= precedence(op))
+            {
+                double b = numStack[nTop--];
+                double a = numStack[nTop--];
+                numStack[++nTop] = apply(a, b, opStack[oTop--]);
+            }
+
+            opStack[++oTop] = op;
+        }
     }
 
-    return result;
+    /* ????????? */
+    while (oTop >= 0)
+    {
+        double b = numStack[nTop--];
+        double a = numStack[nTop--];
+        numStack[++nTop] = apply(a, b, opStack[oTop--]);
+    }
+
+    return numStack[nTop];
 }
